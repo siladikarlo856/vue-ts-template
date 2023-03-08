5c5b1b79ae23dc01be091fd676a17e9cfbdf9f07
siladikarlo856 <siladikarlo856@gmail.com>
2023-03-08 13:22:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-ts-template
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jnvrgh)
---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>Bare minimum Hello World!</template>
+
+<style scoped></style>
